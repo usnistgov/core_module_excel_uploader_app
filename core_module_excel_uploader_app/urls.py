72f5adf982246664d9080fcaa7b72e0132e16e95
Guillaume Sousa Amaral <guillaume.sousa@nist.gov>
2019-09-27 15:38:32 -0400

    feat: upgrade to Django 2.2
    Change-Id: I8d9a81519fc0dfec64ec2d540e47379327883767

--- core_module_excel_uploader_app/urls.py
+++ core_module_excel_uploader_app/urls.py
@@ -1,9 +1,10 @@
 """ Url router for the excel uploader module
 """
-from django.conf.urls import url
+
+from django.urls import re_path
 
 from core_module_excel_uploader_app.views.views import ExcelUploaderModule
 
 urlpatterns = [
-    url(r'module-excel-uploader', ExcelUploaderModule.as_view(), name='core_module_excel_uploader_view'),
+    re_path(r'module-excel-uploader', ExcelUploaderModule.as_view(), name='core_module_excel_uploader_view'),
 ]
